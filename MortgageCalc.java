655a8c1d16fda5a9276f4ed3a1383d52cb629c36
Leo Rand <leopold.rand@gmail.com>
2023-10-15 12:54:23 +0530

    Removed all comments. Split code into different methods

--- MortgageCalc.java
+++ MortgageCalc.java
@@ -10,40 +10,12 @@ public class MortgageCalc {
         float annualInterest = 0;
         byte years = 0;
         
-        // Scanner sc = new Scanner(System.in);
-        
         principal = (int)readNum("Principal: ", 1000, 1_000_000);
-        // while(true) {
-        //     System.out.print("Principal: ");
-        //     principal = sc.nextInt();
-        //     if (principal >= 1000 && principal <= 1_000_000)
-        //         break;
-        //     System.out.println("Enter a value between 1000 and 1,000,000.");
-        // }
         
         annualInterest = (float)readNum("Annual Interest Rate: ", 0, 60);
-        // while(true) {
-        //     System.out.print("Annual Interest Rate: ");
-        //     annualInterest = sc.nextFloat();
-        //     if (annualInterest <= 60 && annualInterest > 0)
-        //         break;
-        //     System.out.println("Enter a value between 0 and 60.");
-        // }
-        
         years = (byte)readNum("Period (Years): ", 0, 30);
-         
-        // while(true) {
-        //     System.out.print("Period (Years): "); 
-        //     years = sc.nextByte();
-        //     if (years > 0 && years <= 30)
-        //         break;
-        //     System.out.println("Enter a value between 0 and 30.");
-        // }
-        
-        // sc.close();
         
         double mortgage = calculateMortgage(principal, annualInterest, years);
-        
         
         String formattedMortgage = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);  
         System.out.println("Mortgage: " + formattedMortgage);
@@ -73,8 +45,6 @@ public class MortgageCalc {
             
             float noOfPayments = years * MONTHS_IN_YEAR;
             float monthlyInterest = (float)(annualInterest / PERCENT / MONTHS_IN_YEAR);
-            
-            //noOfPayments works but monthlyInterest does not - it comes to 0. Changed monthlyInterest to float & it works
 
             double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, noOfPayments)) / (Math.pow(1+ monthlyInterest, noOfPayments)-1);
             
